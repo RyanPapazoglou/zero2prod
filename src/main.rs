1b41a9931d296d56bb84ad1db06689be84cd748c
ryan papazoglou <r.papazoglou@gmail.com>
2022-12-27 14:19:37 -0500

    Add a health check endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+    })
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
