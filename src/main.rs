cdc3df8a1f59b6067df65a961f5f4e18b2045a7a
ryan papazoglou <r.papazoglou@gmail.com>
2023-01-15 17:07:12 -0500

    Add a base application configuration

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use std::net::TcpListener;
+use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to a random port.");
+    let config = get_configuration().expect("Failed to load application config.");
+    let address = format!("127.0.0.1:{}", config.application_port);
+    let listener = TcpListener::bind(address)?;
     run(listener)?.await
 }
